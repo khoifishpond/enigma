0265b55f71a8c4cfb8de0ce0228eb7144e57f2d1
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 19:35:48 -0700

    Update key tests

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -4,14 +4,16 @@ require 'simplecov'
 SimpleCov.start
 
 describe Key do
-  key = Key.new
-
+  before(:each) do
+    @key = Key.new
+  end
+  
   it 'exists' do
-    expect(key).to be_a(Key)
+    expect(@key).to be_a(Key)
   end
 
   it 'has an array of digits' do
-    expect(key.digits).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    expect(@key.digits).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
   end
 
   it 'can generate a default key' do
@@ -21,8 +23,8 @@ describe Key do
   end
 
   it 'can split up the the default key into 4 smaller keys' do
-    keys = double('keys')
+    key = "02715"
 
-    allow(keys).to receive(:keys).and_return([02, 27, 71, 15])
+    expect(@key.keys(key)).to eq([02, 27, 71, 15])
   end
 end
