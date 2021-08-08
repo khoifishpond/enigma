b043fa81aea64ff3306495f6533952dd12215ad3
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 20:05:28 -0700

    Add hash of shifts to test

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -4,19 +4,30 @@ require 'simplecov'
 SimpleCov.start
 
 describe Shift do
-  key = "02715"
-  date = "040895"
-  shift = Shift.new(key, date)
+  keys = [02, 27, 71, 15]
+  offsets = [1, 0, 2, 5]
+  shift = Shift.new(keys, offsets)
 
   it 'exists' do
     expect(shift).to be_a(Shift)
   end
 
-  it 'has a key' do
-    expect(shift.key).to eq("02715")
+  it 'has keys' do
+    expect(shift.keys).to eq([02, 27, 71, 15])
   end
 
-  it 'has a date' do
-    expect(shift.date).to eq("040895")
+  it 'has offsets' do
+    expect(shift.offsets).to eq([1, 0, 2, 5])
+  end
+
+  it 'can create a hash of four shifts' do
+    expected = {
+      A: 3,
+      B: 27,
+      C: 73,
+      D: 20
+    }
+
+    expect(shift.shifts).to eq(expected)
   end
 end
