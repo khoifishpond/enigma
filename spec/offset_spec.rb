875fb9e382825f30169ee3c9bee9dceb93ea7510
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 20:40:29 -0700

    Update ttest with new date method and to allow an argument

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -5,19 +5,23 @@ require 'simplecov'
 SimpleCov.start
 
 describe Offset do
-  offset = Offset.new
+  offset = Offset.new("040895")
 
   it 'exists' do
     expect(offset).to be_a(Offset)
   end
 
-  it 'has a present date' do
+  it 'has a date' do
+    expect(offset.date).to eq("040895")
+  end
+
+  xit 'has a present date' do
     present_date = Date.today.strftime("%d%m%y")
 
     expect(offset.date).to eq(present_date)
   end
 
-  it 'can create four offsets from date' do
+  xit 'can create four offsets from date' do
     test_date = "040895"
 
     expect(offset.offsets(test_date)).to eq([1, 0, 2, 5])
