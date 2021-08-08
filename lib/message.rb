5e90b24f9ec259998385f7330b4de78c4e5b62d6
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 23:54:00 -0700

    Add decrypt method to class

--- lib/message.rb
+++ lib/message.rb
@@ -7,20 +7,32 @@ class Message
   end
 
   def separate
-    @chunks = @content.chars.each_slice(4).map do |array_characters|
+    @content.chars.each_slice(4).map do |array_characters|
       array_characters
     end
   end
 
   def encrypt(shift)
-    something = ""
-    @chunks.each do |chunk|
+    cipher = ""
+    separate.each do |chunk|
       a = @alphabet[(@alphabet.index(chunk[0]) + shift.shifts[:A]) % @alphabet.size] unless chunk[0].nil?
       b = @alphabet[(@alphabet.index(chunk[1]) + shift.shifts[:B]) % @alphabet.size] unless chunk[1].nil?
       c = @alphabet[(@alphabet.index(chunk[2]) + shift.shifts[:C]) % @alphabet.size] unless chunk[2].nil?
       d = @alphabet[(@alphabet.index(chunk[3]) + shift.shifts[:D]) % @alphabet.size] unless chunk[3].nil?
-      something << [a, b, c, d].compact.join
+      cipher << [a, b, c, d].compact.join
     end
-    something
+    cipher
+  end
+
+  def decrypt(shift)
+    message = ""
+    separate.each do |chunk|
+      a = @alphabet[(@alphabet.index(chunk[0]) - shift.shifts[:A]) % @alphabet.size] unless chunk[0].nil?
+      b = @alphabet[(@alphabet.index(chunk[1]) - shift.shifts[:B]) % @alphabet.size] unless chunk[1].nil?
+      c = @alphabet[(@alphabet.index(chunk[2]) - shift.shifts[:C]) % @alphabet.size] unless chunk[2].nil?
+      d = @alphabet[(@alphabet.index(chunk[3]) - shift.shifts[:D]) % @alphabet.size] unless chunk[3].nil?
+      message << [a, b, c, d].compact.join
+    end
+    message
   end
 end
