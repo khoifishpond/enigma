e2e5cb0a1632157989757a350dff4818ae82ec7c
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 19:26:34 -0700

    Add offsets method to class

--- lib/offset.rb
+++ lib/offset.rb
@@ -6,4 +6,9 @@ class Offset
   def initialize
     @date = Date.today.strftime("%d%m%y")
   end
+
+  def offsets(date = @date)
+    date_squared = date.to_i ** 2
+    date_squared.digits.reverse.pop(4)
+  end
 end
