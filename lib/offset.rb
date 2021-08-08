95b8143c8335944fc90f9e00443c46dc076e4c47
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 20:55:04 -0700

    Update offsets method

--- lib/offset.rb
+++ lib/offset.rb
@@ -11,8 +11,8 @@ class Offset
     new(Date.today.strftime("%d%m%y"))
   end
 
-  def offsets(date = @date)
-    date_squared = date.to_i ** 2
+  def offsets
+    date_squared = @date.to_i ** 2
     date_squared.digits.reverse.pop(4)
   end
 end
