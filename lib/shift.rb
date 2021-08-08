9dfb20f66847a79e87e60a65ff35b2cb1cbfa2e4
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 20:05:38 -0700

    Add shifts method to class

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,8 +1,17 @@
 class Shift
-  attr_reader :key, :date
+  attr_reader :keys, :offsets
   
-  def initialize(key, date)
-    @key = key
-    @date = date
+  def initialize(keys, offsets)
+    @keys = keys
+    @offsets = offsets
+  end
+
+  def shifts
+    shifts = {}
+    shifts[:A] = @keys[0] + @offsets[0]
+    shifts[:B] = @keys[1] + @offsets[1]
+    shifts[:C] = @keys[2] + @offsets[2]
+    shifts[:D] = @keys[3] + @offsets[3]
+    shifts
   end
 end
