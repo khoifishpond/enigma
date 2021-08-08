cf5bf093628a9aeba24d4ab4f4dce3f19591e040
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 22:29:34 -0700

    Add content to test

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -4,9 +4,13 @@ require 'simplecov'
 SimpleCov.start
 
 describe Message do
-  message = Message.new
+  message = Message.new("hello world")
 
   it 'exists' do
     expect(message).to be_a(Message)
+  end
+
+  it 'has content' do
+    expect(message.content).to eq("hello world")
   end
 end
