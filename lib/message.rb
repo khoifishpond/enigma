8c213968985f0e7d586a8cc4ab1ba7c385d668c1
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 23:28:15 -0700

    Add encrypt method to class

--- lib/message.rb
+++ lib/message.rb
@@ -1,13 +1,26 @@
 class Message
-  attr_reader :content
+  attr_reader :content, :alphabet
   
   def initialize(content)
     @content = content
+    @alphabet = ("a".."z").to_a << " "
   end
 
   def separate
-    @content.chars.each_slice(4).map do |array_characters|
+    @chunks = @content.chars.each_slice(4).map do |array_characters|
       array_characters
     end
+  end
+
+  def encrypt(shift)
+    something = ""
+    @chunks.each do |chunk|
+      a = @alphabet[(@alphabet.index(chunk[0]) + shift.shifts[:A]) % @alphabet.size] unless chunk[0].nil?
+      b = @alphabet[(@alphabet.index(chunk[1]) + shift.shifts[:B]) % @alphabet.size] unless chunk[1].nil?
+      c = @alphabet[(@alphabet.index(chunk[2]) + shift.shifts[:C]) % @alphabet.size] unless chunk[2].nil?
+      d = @alphabet[(@alphabet.index(chunk[3]) + shift.shifts[:D]) % @alphabet.size] unless chunk[3].nil?
+      something << [a, b, c, d].compact.join
+    end
+    something
   end
 end
