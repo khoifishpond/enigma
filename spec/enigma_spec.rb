ccc0ce907410b5ea78d4b800d3960d6adfe6a866
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 21:29:26 -0700

    Add encrypt a message to test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,4 +9,15 @@ describe Enigma do
   it 'exists' do
     expect(enigma).to be_a(Enigma)
   end
+
+  it 'can encrypt a message' do
+    expected = {
+      # TODO: fix encryption value
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
 end
