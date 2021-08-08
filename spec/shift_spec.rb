ccdaf52f05955249a96309458a84b88d4c7b9c98
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 19:50:40 -0700

    Add date to test

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -5,7 +5,8 @@ SimpleCov.start
 
 describe Shift do
   key = "02715"
-  shift = Shift.new(key)
+  date = "040895"
+  shift = Shift.new(key, date)
 
   it 'exists' do
     expect(shift).to be_a(Shift)
@@ -13,5 +14,9 @@ describe Shift do
 
   it 'has a key' do
     expect(shift.key).to eq("02715")
+  end
+
+  it 'has a date' do
+    expect(shift.date).to eq("040895")
   end
 end
