45bad4a9479efb53f17ec11d18c6c6a7d54c7251
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-08 00:37:13 -0700

    Update methods to create message and shift objects

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,30 +1,36 @@
 require './lib/key'
 require './lib/offset'
+require './lib/shift'
+require './lib/message'
 
 class Enigma
   def encrypt(message, key = Key.generate, date = Offset.generate)
+    message = Message.new(message.downcase)
     if key.class != Key
       key = Key.new(key)
     end
     if date.class != Offset
       offset = Offset.new(date)
     end
+    shift = Shift.new(key.keys, offset.offsets)
     {
-      encryption: "keder ohulw",
+      encryption: message.encrypt(shift),
       key: key.number,
       date: offset.date
     }
   end
 
-  def decrypt(message, key, date = Offset.generate)
+  def decrypt(cipher, key, date = Offset.generate)
+    cipher = Message.new(cipher.downcase)
     if key.class != Key
       key = Key.new(key)
     end
     if date.class != Offset
       offset = Offset.new(date)
     end
+    shift = Shift.new(key.keys, offset.offsets)
     {
-      decryption: "hello world",
+      decryption: cipher.decrypt(shift),
       key: key.number,
       date: offset.date
     }
