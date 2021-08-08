4ed17c1a5b0c7c252e16ac084f9058202ad2835c
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 22:54:15 -0700

    Add separate to test

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -13,4 +13,13 @@ describe Message do
   it 'has content' do
     expect(message.content).to eq("hello world")
   end
+
+  it 'can separate content into arrays of characters' do
+    expected = [
+      ["h", "e", "l", "l"],
+      ["o", " ", "w", "o"],
+      ["r", "l", "d"]
+    ]
+    expect(message.separate).to eq(expected)
+  end
 end
