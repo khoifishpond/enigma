ae39bd07d3a2ccc49ce2a96bf47c09675bf02744
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-08 16:20:02 -0700

    Update classs

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,17 +1,25 @@
 class Shift
-  attr_reader :keys, :offsets
+  attr_reader :key, :offset
   
-  def initialize(keys, offsets)
-    @keys = keys
-    @offsets = offsets
+  def initialize(key, offset)
+    @key = key
+    @offset = offset
+  end
+
+  def keys
+    @key.keys
+  end
+
+  def offsets
+    @offset.offsets
   end
 
   def shifts
     shifts = {}
-    shifts[:A] = @keys[0] + @offsets[0]
-    shifts[:B] = @keys[1] + @offsets[1]
-    shifts[:C] = @keys[2] + @offsets[2]
-    shifts[:D] = @keys[3] + @offsets[3]
+    shifts[:A] = keys[0] + offsets[0]
+    shifts[:B] = keys[1] + offsets[1]
+    shifts[:C] = keys[2] + offsets[2]
+    shifts[:D] = keys[3] + offsets[3]
     shifts
   end
 end
