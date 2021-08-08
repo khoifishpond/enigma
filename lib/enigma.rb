8e145c74dc7b44bddf9452988920df291b6a2e21
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 21:29:40 -0700

    Add encrypt method to class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,18 @@
+require './lib/key'
+require './lib/offset'
+
 class Enigma
-  
+  def encrypt(message, key = Key.generate, date = Offset.generate)
+    if key.class != Key
+      key = Key.new(key)
+    end
+    if date.class != Offset
+      offset = Offset.new(date)
+    end
+    {
+      encryption: "keder ohulw",
+      key: key.number,
+      date: offset.date
+    }
+  end
 end
