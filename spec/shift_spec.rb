54d34cbe216368548d16ca35a435a76c5ef84762
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 19:49:31 -0700

    Add key to test

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -4,9 +4,14 @@ require 'simplecov'
 SimpleCov.start
 
 describe Shift do
-  shift = Shift.new
+  key = "02715"
+  shift = Shift.new(key)
 
   it 'exists' do
     expect(shift).to be_a(Shift)
+  end
+
+  it 'has a key' do
+    expect(shift.key).to eq("02715")
   end
 end
