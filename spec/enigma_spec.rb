6851c66052b66ab9b311b290352448b08ff5b4d1
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-07 21:42:09 -0700

    Add decrypt a message to test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -17,7 +17,18 @@ describe Enigma do
       key: "02715",
       date: "040895"
     }
-    
+
     expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
+  it 'can decrypt a message' do
+    expected = {
+      # TODO: fix decryption value
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
   end
 end
