5f4654dfa225d20d387d6111dea0f642c4eb5605
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-08 20:22:05 -0700

    Add crack method to class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,8 +2,11 @@ require './lib/key'
 require './lib/offset'
 require './lib/shift'
 require './lib/message'
+require './modules/crackable'
 
 class Enigma
+  include Crackable
+  
   def encrypt(message, key = Key.generate, date = Offset.generate)
     message = Message.new(message.downcase)
     key = Key.new(key) if key.class != Key
@@ -25,6 +28,18 @@ class Enigma
       decryption: cipher.decrypt(shift),
       key: key.number,
       date: date.date
+    }
+  end
+
+  def crack(cipher, date = Offset.generate)
+    cipher = Message.new(cipher)
+    date = Offset.new(date) if date.class != Offset
+    key = brute_force(cipher, date)
+    shift = Shift.new(key, date)
+    {
+      decryption: cipher.decrypt(shift),
+      date: date.date,
+      key: key.number
     }
   end
 end
