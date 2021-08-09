c761aacb2725fccf9d2109177d69979fd7c11d33
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-08 18:38:49 -0700

    Move encrypt and decrypt methods to module cryptable

--- lib/message.rb
+++ lib/message.rb
@@ -1,4 +1,7 @@
+require './modules/cryptable'
+
 class Message
+  include Cryptable
   attr_reader :content, :alphabet
   
   def initialize(content)
@@ -10,39 +13,5 @@ class Message
     @content.chars.each_slice(4).map do |array_characters|
       array_characters
     end
-  end
-
-  def encrypt(shift)
-    cipher = ""
-    shifts = shift.shifts.values
-    separate.each do |characters|
-      characters.each_with_index do |character, index|
-        if @alphabet.include?(character)
-          check_index = @alphabet.index(character)
-          rotated_alphabet = @alphabet.rotate(shifts[index])
-          cipher << rotated_alphabet[check_index]
-        else
-          cipher << character
-        end
-      end
-    end
-    cipher
-  end
-
-  def decrypt(shift)
-    message = ""
-    shifts = shift.shifts.values
-    separate.each do |characters|
-      characters.each_with_index do |character, index|
-        if @alphabet.include?(character)
-          check_index = @alphabet.index(character)
-          rotated_alphabet = @alphabet.rotate(-shifts[index])
-          message << rotated_alphabet[check_index]
-        else
-          message << character
-        end
-      end
-    end
-    message
   end
 end
