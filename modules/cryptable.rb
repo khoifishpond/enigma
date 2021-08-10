d6f2a8c99b90816bc6ee7628cdcba8286510b729
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-09 17:25:36 -0700

    Refactor methods

--- modules/cryptable.rb
+++ modules/cryptable.rb
@@ -1,29 +1,11 @@
 module Cryptable
-  def encrypt(shift)
-    cipher = ""
-    shifts = shift.shifts.values
+  def rotate(shifts)
+    message = ""
     separate.each do |characters|
       characters.each_with_index do |character, index|
         if @alphabet.include?(character)
           check_index = @alphabet.index(character)
           rotated_alphabet = @alphabet.rotate(shifts[index])
-          cipher << rotated_alphabet[check_index]
-        else
-          cipher << character
-        end
-      end
-    end
-    cipher
-  end
-
-  def decrypt(shift)
-    message = ""
-    shifts = shift.shifts.values
-    separate.each do |characters|
-      characters.each_with_index do |character, index|
-        if @alphabet.include?(character)
-          check_index = @alphabet.index(character)
-          rotated_alphabet = @alphabet.rotate(-shifts[index])
           message << rotated_alphabet[check_index]
         else
           message << character
@@ -31,5 +13,17 @@ module Cryptable
       end
     end
     message
+  end
+
+  def encrypt(shift)
+    shifts = shift.shifts.values
+    rotate(shifts)
+  end
+
+  def decrypt(shift)
+    shifts = shift.shifts.values.map do |shift|
+      shift * -1
+    end
+    rotate(shifts)
   end
 end
